1c87a459d620cc3adb3670a7ca7c825c06bcc178
Carlos-Robson-Molena <98779457+Carlos-Robson-Molena@users.noreply.github.com>
2022-03-17 01:51:59 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,25 @@
+
+
+# cadastrar
+# atualizar
+# deletar
+# buscar
+
+
+
+while True:
+    print('-=' * 15)
+    print('Sistem de Cadastro Mecanica')
+    print('-=' * 15)
+    print('Cadastrar cliente: 1')
+    print('Buscar Cliente: 2')
+    print('Atualizar Cadastro: 3')
+    print('Deletar Cliente: 4')
+    print('Sair do programa: 5')
+    print('-=' * 15)
+    opc = int(input('Digite a Opção: '))
+    if opc == 5:
+        print('Exit')
+        break
+    elif opc == 1:
+        print()
